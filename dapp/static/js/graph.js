bafb1129e8228161c0c731a20493653575b7a221
Sam <sam@samr1.net>
2017-08-19 13:26:35 +0200

    minor modifications

---
+++ dapp/static/js/graph.js
@@ -0,0 +1,107 @@
+var energyData = [];
+var dataset;
+var totalPoints = 20;
+var updateInterval = 8000;
+var now = new Date().getTime();
+
+var ip = location.host;
+function initData() {
+
+    energyData.shift(); //to remove first item of array
+
+    while (energyData.length < totalPoints) {
+        var y = '';
+        var temp = [now += updateInterval, y]; //data format [x, y]
+
+        energyData.push(temp);
+    }
+
+
+}
+function getData() {
+
+    $.getJSON('http://' + ip  + '/get_data/', function (data) {
+
+        var json = JSON.stringify(data.result);
+        var item = JSON.parse(json);
+        // var energy = [item.timestamp, item.value]; //data format [x, y]
+        var energy = [now += updateInterval, item.value]; //data format [x, y]
+        $('#current').text(item.value);
+
+        if (energyData.length > totalPoints) {energyData.shift();}
+        energyData.push(energy);
+
+    });
+}
+
+var options = {
+    series: {
+        bars: {
+            show: true,
+            barWidth: 8000,
+            align: "right"
+        }
+    },
+    xaxis: {
+        mode: "time",
+        tickSize: [8, "second"],
+        tickFormatter: function (v, axis) {
+
+            var date = new Date(v);
+
+            if (date.getSeconds() % 20 == 0) {
+                var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+                var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+                var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+                return hours + ":" + minutes + ":" + seconds;
+            } else {
+                return "";
+            }
+        },
+        axisLabel: "Time",
+        axisLabelUseCanvas: true,
+        axisLabelFontSizePixels: 14,
+        axisLabelFontFamily: 'Verdana, Arial',
+        axisLabelPadding: 10
+    },
+    yaxis: {
+        min: 0,
+        max: 70,
+        tickSize: 10,
+        tickFormatter: function (v, axis) {
+            if (v % 10 == 0) {
+                return v + " W";
+            } else {
+                return "";
+            }
+        },
+        axisLabel: "Watt",
+        axisLabelUseCanvas: true,
+        axisLabelFontSizePixels: 12,
+        axisLabelFontFamily: 'Verdana, Arial',
+        axisLabelPadding: 6
+    },
+    legend: {
+        labelBoxBorderColor: "#fff"
+    },
+};
+
+$(document).ready(function () {
+    initData();
+
+    dataset = [
+        { data: energyData, color: "#FFA812" }
+    ];
+
+    $.plot($("#flot-placeholder"), dataset, options);
+
+    function update() {
+        getData();
+
+        $.plot($("#flot-placeholder"), dataset, options)
+        setTimeout(update, updateInterval);
+    }
+
+    update();
+});
